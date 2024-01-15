e38d23b84b64a06aa6bb1226363e1267015fddf4
Oleksii Kutsevol <devilout123@gmail.com>
2024-01-15 03:33:14 +0100

    Update Body.js

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -6,7 +6,7 @@ import {useState} from 'react';
 const Body = () => {
 
     const [activeSliderId, setActiveSliderId] = useState('aboutme');
-
+   
 /////////////////
 const data = [
                 {id: 'works', title: 'My Works', body: 'some information about me'}, 
@@ -15,23 +15,21 @@ const data = [
                 {id: 'skills', title: 'My Works', body: 'some information about me'},
             ];
 ////////////////
-const downloadCV = () => {
+
+   const downloadCV = () => {
+    // Путь к вашему файлу
+    const filePath = 'https://github.com/ITE-1/portfolio-resume/blob/main/downloads/CV.pdf';
+    // Создаем ссылку для скачивания
+    const link = document.createElement('a');
+    link.href = filePath;
+    link.download = 'CV.pdf'; // Имя файла для скачивания
+    document.body.appendChild(link);
+    // Запускаем скачивание
+    link.click();
     
-}
-//    const downloadCV = () => {
-//     // Путь к вашему файлу
-//     const filePath = './CV.pdf';
-//     // Создаем ссылку для скачивания
-//     const link = document.createElement('a');
-//     link.href = filePath;
-//     link.download = 'CV.pdf'; // Имя файла для скачивания
-//     document.body.appendChild(link);
-//     // Запускаем скачивание
-//     link.click();
-    
-//     // Удаляем ссылку из DOM
-//     document.body.removeChild(link);
-//    }         
+    // Удаляем ссылку из DOM
+    document.body.removeChild(link);
+   }         
 
     return (
 
@@ -44,7 +42,7 @@ const downloadCV = () => {
                            <div className='body__content__row__window' id='contacts'>Contacts</div>
                            <div className='body__content__row__window' id='about'>
                                 <h1>About Me</h1>
-                                <button onClick={() => downloadCV()}>Download CV</button>
+                                <button onClick={() => downloadCV}>Download CV</button>
                            </div>
                       </div>
                        
