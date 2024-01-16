adcfdb2fd6d7b8bdc6d9f66672879fbdeee193f5
Oleksii Kutsevol <devilout123@gmail.com>
2024-01-16 08:52:53 +0100

    changed Animation
    2

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,12 +5,12 @@ import { useTheme } from '../themeContext/themeContext';
 const Header = () => {
 const {toggleTheme, isDarkMode} = useTheme();
 
-
-const scrollDown = () => {
-    const worksSection = document.getElementById('works'); 
-    worksSection.scrollIntoView({ behavior: 'smooth' });
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
   };
-
 
     return (
             <div className='header'>
@@ -18,16 +18,16 @@ const scrollDown = () => {
                 <button onClick={toggleTheme}>changeTheme</button>
                     <div className='header__row__navigation'>
                         <ul className='header__row__navigation__list'>
-                            <li className='header__row__navigation__item'><a href='#works'>My Works</a></li>
-                            <li className='header__row__navigation__item'><a href='#contacts'>Contacts</a></li>
-                            <li className='header__row__navigation__item'><a href='#about'>About Me</a></li>
+                            <li className='header__row__navigation__item'><div onClick={() => scrollToSection('works')}>My Works</div></li>
+                            <li className='header__row__navigation__item'><div onClick={() => scrollToSection('contacts')}>Contacts</div></li>
+                            <li className='header__row__navigation__item'><div onClick={() => scrollToSection('about')}>About Me</div></li>
                         </ul>
                        
                        
                     </div>
                 </div>
                 <div className={`header__body ${isDarkMode ? 'dark' : 'light'}`}>
-                <div className='header__body__sliderDown' onClick={scrollDown}>slide down</div>
+                <div className='header__body__sliderDown' onClick={() => scrollToSection('about')}>slide down</div>
                 </div>
                 
             </div>
